b436a31b8df956037fefba5e7af85651686070f7
Faith <fmugure@intracitygeeks.org>
2017-08-15 04:13:07 +0000

    update html

--- script.js
+++ script.js
@@ -2,24 +2,20 @@ $(document).ready(function() {
 
       function getQuote() {
 
-        var quote = ["One Time You Pull Out A Thread and Where does it Stop, Where does it Stop?!",
-          "I know we are the chosen people, but every now and then, can't you choose someone else?",
-          "A Blessing on Your Head",
-        ];
-        var author = ["Tevye", "Tevye", "Grandmother Tzeitel"];
+        var quotes = ["One Time You Pull Out A Thread and Where does it Stop, Where does it Stop?!", 
+                      "I know we are the chosen people, but every now and then, can't you choose someone else?", 
+                      "A Blessing on Your Head",
+                     "Even a Poor Tailor Deserves Some Happiness"];
+        var author = ["-Tevye", "-Tevye", "-Grandmother Tzeitel", "-Tzeitel"];
 
-        var randomNumb = Math.floor((Math.random() * quotes.length));
-
+        var randomNum = Math.floor((Math.random()*quotes.length));
         var randomQuote = quotes[randomNum];
-
         var randomAuthor = author[randomNum];
 
-
         $(".quote").text(randomQuote);
-        $(".author").text(author);
-
-        $(".btn").on("click", function() {
+        $(".author").text(randomAuthor);
+            }
+        $(".btn").on("click", function(){
           getQuote();
-
         });
       });
