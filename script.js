138396e86ea425d8387c9504ac9440854304e6ba
Faith <fmugure@intracitygeeks.org>
2017-08-15 04:33:21 +0000

    update

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ $(document).ready(function() {
         var quotes = ["One Time You Pull Out A Thread and Where does it Stop, Where does it Stop?!", 
                       "I know we are the chosen people, but every now and then, can't you choose someone else?", 
                       "A Blessing on Your Head",
-                     "Even a Poor Tailor Deserves Some Happiness"];
+                     "Even a Poor Tailor Deserves Some Happiness!"];
         var author = ["-Tevye", "-Tevye", "-Grandmother Tzeitel", "-Tzeitel"];
 
         var randomNum = Math.floor((Math.random()*quotes.length));
